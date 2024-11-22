2b9eb3df75b69a86e453c6cd81478837fbd99666
Xiaoyu Zhang <zhangxiaoyu6768@163.com>
2024-11-22 14:32:11 +0800

    服务端-服务器搭建

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const multer = require("multer")
+const app = express();
+const port = 8008
+
+
+    / 开放跨域请求
+app.use(function (req, res, next) {
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers", "*");
+    //跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method == "OPTIONS") res.sendStatus(200); //让options尝试请求快速结束
+    else next();
+});
+
+app.use(express.json())
+const update = multer({
+    dest: "./public/upload/temp"
+})
+app.use(update.any())
+
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(port, () => {
+    console.log(`启动成功 : http://localhost:${port}/`)
+})
