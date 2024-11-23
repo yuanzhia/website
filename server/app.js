bedb4941b6a5317fb4e9cdab799fb1e346a79649
Xiaoyu Zhang <zhangxiaoyu6768@163.com>
2024-11-23 20:51:27 +0800

    promise

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
+/**
+"multer"
+"sqlite3"
+"uuid"
+ */
 const express = require("express")
 const multer = require("multer")
+const path = require("path")
 const app = express();
-const port = 8008
+const { db, genid } = require("./db/DbUtils")
+const port = 8080
 
-
-    / 开放跨域请求
+//开放跨域请求
 app.use(function (req, res, next) {
     //设置允许跨域的域名，*代表允许任意域名跨域
     res.header("Access-Control-Allow-Origin", "*");
@@ -21,7 +27,37 @@ const update = multer({
     dest: "./public/upload/temp"
 })
 app.use(update.any())
+//指定静态资源路径
+app.use(express.static(path.join(__dirname, "public")))
 
+//category/_token/add
+const ADMIN_TOKEN_PATH = "/_token"
+app.all("*", async (req, res, next) => {
+    if (req.path.indexOf(ADMIN_TOKEN_PATH) > -1) {
+
+        let { token } = req.headers;
+
+        let admin_token_sql = "SELECT * FROM `admin` WHERE `token` = ?"
+        let adminResult = await db.async.all(admin_token_sql,[token])
+        if(adminResult.err != null || adminResult.rows.length == 0){
+            res.send({
+                code: 403,
+                msg: "请先登录"
+            })
+            return 
+        }else{
+            next()
+        }
+    }else{
+        next()
+    }
+})
+
+app.use("/test", require("./routers/TestRouter"))
+app.use("/admin", require("./routers/AdminRouter"))
+app.use("/category", require("./routers/CategoryRouter"))
+app.use("/blog", require("./routers/BlogRouter"))
+app.use("/upload", require("./routers/UploadRouter"))
 
 app.get("/", (req, res) => {
     res.send("hello world");
@@ -30,3 +66,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`启动成功 : http://localhost:${port}/`)
 })
+
